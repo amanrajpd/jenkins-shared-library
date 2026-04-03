397b2b669bf25e46f0cdad7dc0697d0c4653cb1b
amanrajpd <134468196+amanrajpd@users.noreply.github.com>
2026-04-03 19:56:09 +0530

    Create push_image.groovy

---
+++ vars/push_image.groovy
@@ -0,0 +1,11 @@
+def call(String Project , String Imagetag , String dockerhubuser)
+{
+  echo "pushing image to docker hub"
+                withCredentials([usernamePassword(
+                    credentialsId:"docker-hub-cred",
+                    usernameVariable:"dockerHubUser", 
+                    passwordVariable:"dockerHubPass")]){
+                sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+                sh "docker image tag notes-app:latest ${dockerHubUser}/${Project}:${Imagetag}"
+                sh "docker push ${dockerHubUser}/${Project}:${Imagetag}"
+}
