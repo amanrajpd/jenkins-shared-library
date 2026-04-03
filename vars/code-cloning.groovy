6d7802ec5fe53046a8aae86bc927d754f017f07c
amanrajpd <134468196+amanrajpd@users.noreply.github.com>
2026-04-03 19:30:19 +0530

    Update code-cloning.groovy

--- vars/code-cloning.groovy
+++ vars/code-cloning.groovy
@@ -1,6 +1,6 @@
 def call(String url , String branch)
 {
   echo "Code cloning from github"
-  git url: "${url}" , branch:"${branch}"
+  git url: "${url}" , branch: "${branch}"
   echo "Code cloning completed"
 }
